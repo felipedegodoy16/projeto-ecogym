8dda938058d64c07c524d916c98313e1f6177fca
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-11-30 18:32:02 -0300

    remove exercise implemented

--- apis/prac/alter.php
+++ apis/prac/alter.php
@@ -12,6 +12,39 @@
 
   $res = 0;
 
+  // Delete Exer
+  $sql = "SELECT * FROM exercicio WHERE FK_TREINO_ID = :id_prac;";
+
+  // Conectando ao banco e preparando a query
+  $stmt = ConnectionFactory::getConnection()->prepare($sql);
+
+  $stmt->bindValue(":id_prac", $datas['practice']['id'], PDO::PARAM_INT);
+
+  $stmt->execute() or die(print_r($stmt->errorInfo(), true));
+  $allExercises = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach($allExercises as $exDb) {
+    $dif = 0;
+    foreach($datas['exercises'] as $ex) {
+      if(intval($ex['id']) === $exDb['ID_EXERCICIO']) {
+        $dif = 1;
+        continue;
+      }
+    }
+    if(!$dif) {
+      $sql = "DELETE FROM exercicio WHERE ID_EXERCICIO = :id_exer;";
+
+      // Conectando ao banco e preparando a query
+      $stmt = ConnectionFactory::getConnection()->prepare($sql);
+
+      $stmt->bindValue(":id_exer", $exDb['ID_EXERCICIO'], PDO::PARAM_INT);
+
+      $stmt->execute() or die(print_r($stmt->errorInfo(), true));
+
+      $res++;
+    }
+  }
+
   foreach($datas['exercises'] as $exer) {
     if($exer['id']) {
       $sql = "UPDATE exercicio SET EXERCICIO = :name_exer, SERIES = :series, REPETICOES = :reps, CARGA = :charge WHERE ID_EXERCICIO = :id;";
